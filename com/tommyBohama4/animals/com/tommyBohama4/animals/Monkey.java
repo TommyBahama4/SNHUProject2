c41cc57c0e8d8e45cba96a37196f71deab52f4f8
TommyBahama4 <Thomas.waters@outlook.com>
2023-08-12 09:54:11 -0400

    Changed Notes for monkey constructor.

--- com/tommyBohama4/animals/com/tommyBohama4/animals/Monkey.java
+++ com/tommyBohama4/animals/com/tommyBohama4/animals/Monkey.java
@@ -6,23 +6,7 @@ public class Monkey extends RescueAnimal {
     private String height;
     private String bodyLength;
 
-    /**
-     * Default constructor for monkey
-     * 
-     * @param name
-     * @param gender
-     * @param age
-     * @param weight
-     * @param acquisitionDate
-     * @param acquisitionCountry
-     * @param trainingStatus
-     * @param reserved
-     * @param inServiceCountry
-     * @param species
-     * @param tailLength
-     * @param height
-     * @param bodyLength
-     */
+    // Constructor
     public Monkey(String name, String gender, String age,
             String weight, String acquisitionDate, String acquisitionCountry,
             String trainingStatus, boolean reserved, String inServiceCountry,
@@ -37,7 +21,7 @@ public class Monkey extends RescueAnimal {
     }
 
     /**
-     * @return the species of monkey
+     * @return String returns the species of monkey
      */
     public String getSpecies() {
         return species;
@@ -50,26 +34,50 @@ public class Monkey extends RescueAnimal {
         this.species = species;
     }
 
+    /**
+     * 
+     * @return length of monkey tail
+     */
     public String getTailLength() {
         return tailLength;
     }
 
+    /**
+     * 
+     * @param tailLength the length of the tail
+     */
     public void setTailLength(String tailLength) {
         this.tailLength = tailLength;
     }
 
+    /**
+     * 
+     * @return Monkey height
+     */
     public String getHeight() {
         return height;
     }
 
+    /**
+     * 
+     * @param height height of the monkey
+     */
     public void setHeight(String height) {
         this.height = height;
     }
 
+    /**
+     * 
+     * @return the monkey's body length
+     */
     public String getBodyLength() {
         return bodyLength;
     }
 
+    /**
+     * 
+     * @param bodyLength length of the body
+     */
     public void setBodyLength(String bodyLength) {
         this.bodyLength = bodyLength;
     }
